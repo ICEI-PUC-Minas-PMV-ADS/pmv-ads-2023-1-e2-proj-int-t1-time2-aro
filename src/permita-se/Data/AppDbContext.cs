b97c88c20af65810dca1397629c91dec08a4665d
lucasmarcelods <89558202+lucasmarcelods@users.noreply.github.com>
2023-04-17 23:30:03 -0300

    Adição do model categoria e relacionamento categoria-produto.

--- src/permita-se/Data/AppDbContext.cs
+++ src/permita-se/Data/AppDbContext.cs
@@ -1,14 +1,17 @@
-﻿using System;
+﻿
 using Microsoft.EntityFrameworkCore;
-
+using permita_se.Models;
 
 namespace permita_se.Data
 {
-    public class AppDbContext:DbContext
+    public class AppDbContext : System.Data.Entity.DbContext
     {
-        public AppDbContext(DbContextOption<AppDbContext> options) : base(options)
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
-
         }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+       
     }
+    
 }
--- src/permita-se/Models/Produto.cs
+++ src/permita-se/Models/Produto.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace permita_se.Models
 {
@@ -11,4 +13,8 @@ namespace permita_se.Models
         public double Preco { get; set; }
         public string ImagemUrl { get; set; }
     }
+
+    public int CategoriaId { get; set; }
+    [ForeignKey("CategoriaId")]
+    public Categoria Categoria { get; set; }
 }
