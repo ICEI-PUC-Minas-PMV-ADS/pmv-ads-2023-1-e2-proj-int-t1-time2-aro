66878d12d1afc397f1a2db55d4cf16ff5517396d
Nayra <nayra.a.reis@gmail.com>
2023-05-19 17:31:51 -0300

    adicionando visão do usuario #erro

---
+++ src/permita-se/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using permita_se.Data;
+using permita_se.Data.ViewModel;
+using permita_se.Migrations;
+using System;
+
+namespace permita_se.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private readonly UserManager<Usuario> _userManager;
+        private readonly SignInManager<Usuario> _signInManager;
+        private readonly PermitaSeDbContext _context;
+
+        public AccountController(UserManager<Usuario> userManager, SignInManager<Usuario> signInManager, PermitaSeDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager ?? throw new ArgumentNullException(nameof(signInManager));
+            _context = context;
+        }
+
+
+        public IActionResult Login() =>View(new LoginVM());
+      
+    }
+}
