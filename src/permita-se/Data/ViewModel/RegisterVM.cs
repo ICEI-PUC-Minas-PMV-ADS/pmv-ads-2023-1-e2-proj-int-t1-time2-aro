c57f3c9666b3ed2b1237c9e75e3046638c3b2ff9
Gabriel <gabriel9lourenco@gmail.com>
2023-06-01 01:13:29 -0300

    Ajustes na tela de usuarios e nas validacoes de cadastro

--- src/permita-se/Data/ViewModel/RegisterVM.cs
+++ src/permita-se/Data/ViewModel/RegisterVM.cs
@@ -11,14 +11,17 @@ namespace permita_se.Data.ViewModel
         public string Sobrenome { get; set; }
 
         [Required(ErrorMessage = "Telefone é necessário")]
+        [RegularExpression(@"^\(?[1-9]{2}\)? ?(?:[2-8]|9[1-9])[0-9]{3}\-?[0-9]{4}$", ErrorMessage = "Telefone inválido")]
         public string Telefone { get; set; }
 
         [Display(Name = "E-mail")]
         [Required(ErrorMessage = "E-mail é necessário")]
+        [EmailAddress(ErrorMessage = "E-mail inválido")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Senha é necessária")]
         [DataType(DataType.Password)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{6,15}$", ErrorMessage = "A senha deve conter no mínimo 6 caracteres, uma letra maiúscula, uma minúscula, um número e um caractere especial.")]
         public string Senha { get; set; }
 
 
