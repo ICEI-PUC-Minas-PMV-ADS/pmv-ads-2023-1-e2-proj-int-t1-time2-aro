4891fb709d079a904580d1a0e5f36177fcd6d21f
Gabriel <gabriel9lourenco@gmail.com>
2023-06-03 00:21:54 -0300

    Ajustes no link de direcionamento para o whatsapp

--- src/permita-se/Controllers/PedidosController.cs
+++ src/permita-se/Controllers/PedidosController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using permita_se.Data.Carrinho;
 using permita_se.Data.Services;
@@ -6,11 +7,10 @@ using permita_se.Data.ViewModel;
 using permita_se.Model;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+using System.Globalization;
+using System.Numerics;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using System.Web;
 
 namespace permita_se.Controllers
 {
@@ -20,12 +20,14 @@ namespace permita_se.Controllers
         private readonly IProdutoService _produtoService;
         private readonly CarrinhoDeCompra _carrinhoDeCompra;
         private readonly IPedidosService _pedidosService;
+        private readonly UserManager<Usuario> _userManager;
 
-        public PedidosController(IProdutoService produtoService, CarrinhoDeCompra carrinhoDeCompra, IPedidosService pedidosService)
+        public PedidosController(IProdutoService produtoService, CarrinhoDeCompra carrinhoDeCompra, IPedidosService pedidosService, UserManager<Usuario> userManager)
         {
             _produtoService = produtoService;
             _carrinhoDeCompra = carrinhoDeCompra;
             _pedidosService = pedidosService;
+            _userManager = userManager;
         }
 
         public async Task<IActionResult> Index()
@@ -80,25 +82,43 @@ namespace permita_se.Controllers
 
             await _pedidosService.CriarPedidoAsync(items, IdUsuario);
 
-            string texto = "Olá! Vim do site e desejo receber atendimento. \nPedido:";       
+            var link = GetLinkRedirecionamento(IdUsuario, items);
 
-            foreach (var item in items)
-            {
-                    texto += 
-                    " \nNome do Produto: " + item.Produto.Nome +
-                    " \nQuantidade: " + item.Quantidade +
-                    " \nValor: " + item.Produto.Preco;
-            }
-
-            texto += " \nTotal Da Compra: " + _carrinhoDeCompra.GetCarrinhoTotal();
             await _carrinhoDeCompra.LimparCarrinhoDeCompraAsync();
 
             ViewBag.HideCarrinho = true;
+            return View(model: link);
+        }
+
+        private string GetLinkRedirecionamento(string IdUsuario, List<CarrinhoItem> items)
+        {
+            var user = _userManager.FindByIdAsync(IdUsuario).Result;
+
+            string texto = "Olá! Gostaria de finalizar o meu pedido. Seguem os detalhes:" +
+                "\n\nCliente: " + user.Nome + " " + user.Sobrenome +
+                "\nTelefone de Contato: " + string.Format("{0:(##) #####-####}", user.Telefone) +
+                "\n\nProdutos:";
+
+            int i = 1;
+
+            foreach (var item in items)
+            {
+                texto +=
+                "\n" + i + ". " + item.Produto.Nome +
+                "\nQuantidade: " + item.Quantidade +
+                "\nValor: " + (item.Produto.Preco * item.Quantidade).ToString("c", CultureInfo.CreateSpecificCulture("pt-BR")) +
+                "\n";
+                i++;
+            }
+
+            texto += " \nTotal: " + _carrinhoDeCompra.GetCarrinhoTotal().ToString("c", CultureInfo.CreateSpecificCulture("pt-BR")) +
+                "\n\nAguardo confirmação e informações sobre a disponibilidade do produto e o prazo de entrega." +
+                "\nObrigado!";
 
             string textoCodificado = Uri.EscapeDataString(texto);
             string link = "https://wa.me/5531982493554?text=" + textoCodificado;
 
-            return View();
+            return link;
         }
     }
 }
