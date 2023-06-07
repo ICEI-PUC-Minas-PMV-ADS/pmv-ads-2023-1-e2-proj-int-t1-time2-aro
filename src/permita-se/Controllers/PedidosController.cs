6bad04a1e5797fcf89edee4bba3faec0e7db68a8
Gabriel <gabriel9lourenco@gmail.com>
2023-06-06 21:32:27 -0300

    Correção na action de adicionar na view de detalhes

--- src/permita-se/Controllers/PedidosController.cs
+++ src/permita-se/Controllers/PedidosController.cs
@@ -9,7 +9,6 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using System.Numerics;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
